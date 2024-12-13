d02d8310de9fa7873e502b6602ea068104319ae4
和田皓翔 <wadahiroka@192.168.0.10>
2024-12-13 15:45:31 +0900

    fix

--- nvim/lua/astronvim/autocmds.lua
+++ nvim/lua/astronvim/autocmds.lua
@@ -24,6 +24,29 @@ autocmd("BufReadPre", {
   end,
 })
 
+autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+    local params = vim.lsp.util.make_range_params()
+    params.context = {only = {"source.organizeImports"}}
+    -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+    -- machine and codebase, you may want longer. Add an additional
+    -- argument after params if you find that you have to write the file
+    -- twice for changes to be saved.
+    -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
+        end
+      end
+    end
+    vim.lsp.buf.format({async = false})
+  end
+})
+
 local terminal_settings_group = augroup("terminal_settings", { clear = true })
 -- TODO: drop when dropping support for Neovim v0.9
 if vim.fn.has "nvim-0.9.4" == 0 then
---
+++ nvim/lua/plugins/configs/autocommands.lua
@@ -0,0 +1,11 @@
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*.go',  
+  callback = function()
+    vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
+  end
+})im.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*.go',  
+  callback = function()
+    vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
+  end
+})
